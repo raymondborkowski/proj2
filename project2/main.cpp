931d9c4de72d3e1876909202acc7b3b221e0bb1b
raymondborkowski <rborkows@umich.edu>
2014-10-07 23:39:40 -0400

    Added some get line shit

--- project2/main.cpp
+++ project2/main.cpp
@@ -9,16 +9,25 @@
 #include <iostream>
 #include "functions.h"
 #include <fstream>
+#include <sstream>
 
 
 int main(int argc, char ** argv)
 {
     bool poor_man = false, binary=false, sorted = false, pairing = false;
-    string fileName;
-    fileName = getopt(argc, argv, poor_man, binary, sorted, pairing);
-    ifstream b_file ("/Users/RJB/Documents/classes/eecs281/project2/project2/" + fileName);
+    string temp;
+    temp = getopt(argc, argv, poor_man, binary, sorted, pairing);
+    ifstream b_file ("/Users/RJB/Documents/classes/eecs281/project2/project2/" + temp);
+    getline(b_file, temp);
+    string inputType = temp;
+    cout<<inputType;
     
-    
+    if(inputType == "M"){
+        
+    }
+    else if(inputType == "R"){
+        
+    }
     
     
     return 0;
