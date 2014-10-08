c595dc1071e545b5cca8910a194c9ca5c8ae0998
raymondborkowski <rborkows@umich.edu>
2014-10-07 21:06:15 -0400

    same as last commit still need to check for empty file

--- project2/main.cpp
+++ project2/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char ** argv)
     bool poor_man = false, binary=false, sorted = false, pairing = false;
     string fileName;
     fileName = getopt(argc, argv, poor_man, binary, sorted, pairing);
-    ifstream b_file ("/Users/RJB/Documents/classes/eecs281/project2/" + fileName);
+    ifstream b_file ("/Users/RJB/Documents/classes/eecs281/project2/project2/" + fileName);
     
     
     
