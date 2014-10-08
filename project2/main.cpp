029d2b421d1ab0aa88ba98195c6659bb344824c9
raymondborkowski <rborkows@umich.edu>
2014-10-07 21:02:03 -0400

    Initial Commit

---
+++ project2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  project2
+//
+//  Created by Raymond Borkowski on 10/7/14.
+//  Copyright (c) 2014 Raymond Borkowski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
